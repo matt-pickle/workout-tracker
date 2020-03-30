b9bff26669a0bcd752929268139e09bfa10e7652
Matt Pickle <mpickle86@gmail.com>
2020-03-30 18:15:34 -0500

    Updates

---
+++ precache-manifest.fb9fcdadcec8884c4e0cf7fa76b52f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2a62d2dfff6227156b021ec2a90afd6",
+    "url": "/workout-tracker/index.html"
+  },
+  {
+    "revision": "0a0dd0ccfb4953eed748",
+    "url": "/workout-tracker/static/css/main.dbaeb7aa.chunk.css"
+  },
+  {
+    "revision": "58564e9e5b2b8358310d",
+    "url": "/workout-tracker/static/js/2.76257cc3.chunk.js"
+  },
+  {
+    "revision": "44bb13c376a360a26522c8368cf779a1",
+    "url": "/workout-tracker/static/js/2.76257cc3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a0dd0ccfb4953eed748",
+    "url": "/workout-tracker/static/js/main.56d17819.chunk.js"
+  },
+  {
+    "revision": "77413a1389e02843ed17",
+    "url": "/workout-tracker/static/js/runtime-main.1d69f642.js"
+  }
+]);
