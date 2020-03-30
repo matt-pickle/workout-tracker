2c738e0f9f0b4bb9385c9a6adbf5ae87c24716bf
Matt Pickle <mpickle86@gmail.com>
2020-03-30 18:38:18 -0500

    Fix empty History page bug

--- src/Components/History.js
+++ src/Components/History.js
@@ -5,15 +5,17 @@ import PastWorkout from "./PastWorkout";
 function History() {
   const workoutHistory = JSON.parse(localStorage.getItem("workoutHistory"));
 
-  const pastWorkouts = workoutHistory.map(workout => {
-    const workoutDate = Object.keys(workout)
-    console.log(workout.workoutDate)
-    return (
-      <PastWorkout date={workoutDate}
-                   workoutArr={workout[workoutDate]}
-      />
-    )
-  })
+  const pastWorkouts = workoutHistory ? 
+    workoutHistory.map(workout => {
+      const workoutDate = Object.keys(workout)
+      return (
+        <PastWorkout date={workoutDate}
+                    workoutArr={workout[workoutDate]}
+        />
+      )
+    })
+  : <h1>No workout history yet!</h1>;
+
   
   return (
     <div className="history">
