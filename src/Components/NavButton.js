6c4fed2a99fcf9ec99fda36e860537b3f7e66284
Matt Pickle <mpickle86@gmail.com>
2020-04-02 17:08:19 -0500

    Disable hover effect for NavButton on mobile

--- src/Components/NavButton.js
+++ src/Components/NavButton.js
@@ -5,8 +5,15 @@ import "../Styles/NavButton.css";
 function NavButton(props) {
   const [hoverClass, setHoverClass] = useState("");
 
+  //Applies hover effect class if not using a touch screen
   function hover() {
-    setHoverClass("hovered");
+    if ("ontouchstart" in window
+      || navigator.maxTouchPoints > 0
+      || navigator.msMaxTouchPoints > 0) {
+        setHoverClass("")
+    } else {
+      setHoverClass("hovered");
+    }
   }
 
   function unHover() {
